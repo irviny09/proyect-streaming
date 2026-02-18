8dffb4dfac814464813fc8cd407f393e8f23e835
irvin <marquez190705@gmail.com>
2026-02-17 22:14:43 -0600

    Primer comit

---
+++ src/main/java/com/ubam/proyecto_parcial1/ProyectoParcial1Application.java
@@ -0,0 +1,13 @@
+package com.ubam.proyecto_parcial1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoParcial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoParcial1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubam/proyecto_parcial1/ProyectoParcial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubam.proyecto_parcial1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoParcial1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
